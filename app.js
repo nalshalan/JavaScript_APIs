efc143683b73954ee9adf883f75551b94f9b6f8f
Nawaf Alshalan <noufi02@icloud.com>
2024-07-30 15:44:42 -0400

    Finished JS Fetch API

--- app.js
+++ app.js
@@ -1,4 +1,23 @@
 console.log("Hello World!\n==========\n");
 
-// Exercise 1 Section
-console.log("EXERCISE 1:\n==========\n");
+const searchButton = document.getElementById("submitSearch");
+const searchInput = document.getElementById("searchWord");
+const feedbackElement = document.getElementById("feedback");
+const imgElement = document.querySelector("#imageContainer > img");
+
+const GIPHY_URL = "https://api.giphy.com/v1/gifs/translate";
+const GIPHY_KEY = "fc2f9Fl5aL4GnERdhcss8KaREQ4cp74i";
+
+searchButton.addEventListener("click", () => {
+  fetch(`${GIPHY_URL}?api_key=${GIPHY_KEY}&s=${searchInput.value}`)
+    .then((response) => response.json())
+    .then((res) => {
+        feedbackElement.textContent = "";
+        imgElement.src = res.data.images.original.url;
+        searchInput.value = "";
+    })
+    .catch((err) => {
+        console.error(err);
+        feedbackElement.textContent = err.message;
+    });
+});
